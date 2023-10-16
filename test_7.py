eeb17677187d9daacf41b4e9206c567c7d54e6b3
manishchenko <maxxanishenko@gmail.com>
2023-10-16 16:24:49 +0300

    Create 'test_7.py' with func 'create_csv'

---
+++ test_7.py
@@ -0,0 +1,38 @@
+from string import ascii_letters
+import random as rn
+
+
+def create_csv(count_rows, header):
+    records = []
+
+    if count_rows > 10**9:
+        count_rows = 10**9
+
+    for _ in range(count_rows):
+        current_row = []
+
+        for value in header.values():
+            if value == "int":
+                element = str(rn.randint(0, 100))
+            elif value == "str":
+                element = str("".join(rn.choices(ascii_letters, k=rn.randint(1, 100))))
+            elif value == "bool":
+                element = str(rn.choice([True, False]))
+            current_row.append(element)
+
+        records.append(current_row)
+
+    try:
+        with open("file.csv", "w") as file:
+            file.write(";".join(header.keys()) + "\n")
+
+            for row in records:
+                file.write(";".join(row) + "\n")
+    except Exception:
+        print("Проблемы с записью в файл")
+
+
+N = 1000
+header = {"column1": "int", "column2": "str", "column3": "bool", "column4": "int"}
+
+create_csv(N, header)
