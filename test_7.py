6ab70bb06087b329819cb06beada6fb7d59833c1
manishchenko <maxxanishenko@gmail.com>
2023-10-17 01:31:30 +0300

    add description and some styles fixes

--- test_7.py
+++ test_7.py
@@ -1,16 +1,27 @@
+"""
+Реализовать решение для генерации случайных данных в формат csv. 
+На входе:
+N - количество строк необходимое для генерации
+header - словарь ключом которого является название колонки, а значением один из типов: int, str или bool. 
+Файл обязательно должен содержать заголовок.
+
+Кол-во строк ограничено: 10**9
+Для генерации типа str длина текста не должна превышать > 100.
+Для генерации типа int: диапазон значений от 0 до 100, целочисленные.
+"""
+
+
 from string import ascii_letters
 import random as rn
 
 
 def create_csv(count_rows, header):
     records = []
-
     if count_rows > 10**9:
         count_rows = 10**9
 
     for _ in range(count_rows):
         current_row = []
-
         for value in header.values():
             if value == "int":
                 element = str(rn.randint(0, 100))
@@ -19,7 +30,6 @@ def create_csv(count_rows, header):
             elif value == "bool":
                 element = str(rn.choice([True, False]))
             current_row.append(element)
-
         records.append(current_row)
 
     try:
